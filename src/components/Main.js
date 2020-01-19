fdfd3a793b07638eeac8d905e76798a137e9986b
Jonazao <jhonnatanguerrerom@gmail.com>
2020-01-18 21:38:43 -0800

    Token Handler

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
 //Import Modules
 import React, { Component, Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
-
+import store from "./../store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./../utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./../actions/authActions";
 //Import Components
 import Navbar from "./Layout/LayoutNav";
 import Footer from "./Layout/LayoutFooter";
@@ -9,7 +12,29 @@ import Landing from "./Landing";
 import Clinic from "./Clinic/Clinic";
 import Appointment from "./Appointment";
 //Imprt Routes
-import { MAIN_APP_URL, CLINICS_URL } from "./../routes";
+import { MAIN_APP_URL, CLINICS_URL, LOGIN_URL } from "./../routes";
+//Check For Token
+if (localStorage.jwt) {
+  //Set Auth Token header Auth
+  setAuthToken(localStorage.jwt);
+  //Decode the Token and get Info
+  const decoded = jwt_decode(localStorage.jwt);
+  //Set User and isAuthenticated
+  const { email, id } = decoded;
+  const user = {
+    id,
+    email
+  };
+  store.dispatch(setCurrentUser(user));
+  //Check if the Token expired
+  const currentTime = Date.now() / 1000;
+  if (!decoded || decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //Redirect to Login
+    window.location.href = LOGIN_URL;
+  }
+}
 
 class Main extends Component {
   render() {
