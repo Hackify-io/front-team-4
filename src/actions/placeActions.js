6aff3a5e5b01cc8e373065adbd4a7b2122a0c0ef
Gustavo Adolfo Loera Vazquez <gustavo.loera@softtek.com>
2020-01-28 08:52:07 -0800

    null check on places

--- src/actions/placeActions.js
+++ src/actions/placeActions.js
@@ -4,12 +4,14 @@ import PlaceService from './../services/placeService';
 // Get Places
 export const getPlaces = () => async dispatch => {
   const places = await PlaceService.getPlaces();
-  const appPlaces = places.map(p => {
-    return {
-      place: p,
-      display: `${p.city} ${p.state} - ${p.country}`
-    };
-  });
+  const appPlaces = places
+    ? places.map(p => {
+        return {
+          place: p,
+          display: `${p.city} ${p.state} - ${p.country}`
+        };
+      })
+    : null;
   dispatch({
     type: GET_PLACES,
     payload: appPlaces
