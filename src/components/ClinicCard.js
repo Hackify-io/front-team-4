7c2a7f78ee978da87d17b6af3b2c7c6b680c6cb9
Jonazao <jhonnatanguerrerom@gmail.com>
2020-01-30 11:09:48 -0800

    Add Client Card and footer

---
+++ src/components/ClinicCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Card, CardTitle, Icon } from 'react-materialize';
+export default class ClinicCard extends Component {
+  render() {
+    const { clinic } = this.props;
+    return (
+      <Row>
+        <Col s={12}>
+          <Card
+            actions={[<Link to={`/clinic/${clinic._id}`}>Visit</Link>]}
+            closeIcon={<Icon>close</Icon>}
+            header={
+              <CardTitle
+                style={{ maxWidth: '300px' }}
+                image="https://materializecss.com/images/sample-1.jpg"
+              />
+            }
+            horizontal
+            revealIcon={<Icon>more_vert</Icon>}
+          >
+            <h3>{clinic.name}</h3>
+            {clinic.description}
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Footer } from 'react-materialize';
 
 class LayoutFooter extends Component {
   render() {
-    return <div>Im the footer</div>;
+    return (
+      <Footer className="example" copyrights="&MedTravel 2020">
+        <h5 className="white-text">Site still under construction</h5>
+        <p className="grey-text text-lighten-4">
+          Please be patient, the site is under construction
+        </p>
+      </Footer>
+    );
   }
 }
 
