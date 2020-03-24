767aadf27fbd931efb6b4c66c82dd763bffc4e3c
Jhonnatan Guerrero <jhonnatanguerrerom@gmail.com>
2020-03-23 23:58:39 -0700

    New Footer

--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
-import { Container, Segment, Grid, List } from 'semantic-ui-react';
+import { Container, Segment, Grid, List, Header, Input, Icon, Responsive } from 'semantic-ui-react';
 
 class LayoutFooter extends Component {
+  state = {};
+  handleOnUpdate = (e, {width}) => this.setState({width});
   render() {
+    const {width} = this.state;
+    const textAlign = width <= Responsive.onlyMobile.maxWidth ? 'center' : '';
+    console.log(width, textAlign, Responsive.onlyMobile.maxWidth);
     return (
-      <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
+      <Responsive 
+        as={Segment}
+        inverted vertical style={{ padding: '5em 0em' }}
+        onUpdate={this.handleOnUpdate}
+      >
+        <Container>
+          <Grid textAlign={textAlign} inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={7}>           
+                <Grid textAlign={textAlign} inverted stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      (LOGO) MED TRAVEL
+                    </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Grid.Column width={7}>
+                      <Header inverted as='h4' content='FOLLOW US' />
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <Icon name="twitter"/>
+                        <Icon name="facebook f" />
+                        <Icon name="youtube"/>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Header inverted as='h4' content='Useful links' />
+                <List link inverted>
+                  <List.Item as='a'>About</List.Item>
+                  <List.Item as='a'>Login</List.Item>
+                  <List.Item as='a'>Register</List.Item>
+                  <List.Item as='a'>Contacts</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={4}>
+              <Header inverted as='h4' content='Contact with us' />
+                <List link inverted>
+                  <List.Item as='a'>+61 154515451545</List.Item>
+                  <List.Item as='a'>info@medtravel.com</List.Item>
+                </List>
+              <Header inverted as='h4' content='Newsletter' />
+              <Input
+                action={{
+                  color: 'teal',
+                  content: 'Submit',
+                }}
+                actionPosition='right'
+                placeholder='Your email'
+              />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>          
+      </Responsive>
     );
   }
 }
