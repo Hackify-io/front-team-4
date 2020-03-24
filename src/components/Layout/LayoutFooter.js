9830b17f95484df149752905cd71a8ea42702c0d
Jhonnatan Guerrero <jhonnatanguerrerom@gmail.com>
2020-03-23 22:34:14 -0700

    New Footer

--- src/components/Layout/LayoutFooter.js
+++ src/components/Layout/LayoutFooter.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import { Footer } from 'react-materialize';
+import { Container, Segment, Grid, List } from 'semantic-ui-react';
 
 class LayoutFooter extends Component {
   render() {
     return (
-      <Footer className="example" copyrights="&MedTravel 2020">
-        <h5 className="white-text">Site still under construction</h5>
-        <p className="grey-text text-lighten-4">
-          Please be patient, the site is under construction
-        </p>
-      </Footer>
+      <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Sitemap</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>Religious Ceremonies</List.Item>
+                <List.Item as='a'>Gazebo Plans</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Services' />
+              <List link inverted>
+                <List.Item as='a'>Banana Pre-Order</List.Item>
+                <List.Item as='a'>DNA FAQ</List.Item>
+                <List.Item as='a'>How To Access</List.Item>
+                <List.Item as='a'>Favorite X-Men</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>
+                Footer Header
+              </Header>
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
     );
   }
 }
